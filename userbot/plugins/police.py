f8b6c6c1dcc4bffca331b5d3fc105b242e059e13
JAISHNAVPRASAD-DarklIous <62203009+JAISHNAVPRASAD-DarklIous@users.noreply.github.com>
2020-04-24 20:25:58 +0530

    Update police.py

--- userbot/plugins/police.py
+++ userbot/plugins/police.py
@@ -65,19 +65,45 @@ async def _(event):
             "`Dectecting User`"
             
         ]
-            if idd == 767014786:
-            await reply_message.reply("`Wait a second, This is my master!`\n**How dare you threaten to ban my master nigger!**\n\n__Your account has been hacked! Pay 69$ to my master__ [Anubis](tg://user?id=742506768) __to release your account__😏")
+             mentions = "`Warning!! User DMCAed By BOSS @CyberJalagam\n`"
+    no_reason = "__Reason: Kang Without Permission. __"
+    await event.edit("**Notice Preparation In Progress❗️⚜️☠️**")
+    asyncio.sleep(3.5)
+    chat = await event.get_input_chat()
+    async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
+        mentions += f""
+    reply_message = None
+    if event.reply_to_msg_id:
+        reply_message = await event.get_reply_message()
+        replied_user = await event.client(GetFullUserRequest(reply_message.from_id))
+        firstname = replied_user.user.first_name
+        usname = replied_user.user.username
+        idd = reply_message.from_id
+        # make meself invulnerable cuz why not xD
+        if idd == 767014786:
+            await reply_message.reply("`Wait a second, This is my master!`\n**How dare you threaten to ban my master nigger!**\n\n__Your account has been hacked! Pay 50$ to my master__ @CyberJalagam __to release your account__😏")
         else:
             jnl=("`Warning!! `"
                   "[{}](tg://user?id={})"
-                  "` Takedown By @CyberJalagam ...\n\n`"
-                  "**First Name: ** __{}__\n"
+                  "` Warning!! User DMCAed By BOSS @CyberJalagam\n\n`"
+                  "**Kanger's Name: ** {}\n"
                   "**ID : ** `{}`\n"
                 ).format(firstname, idd, firstname, idd)
             if usname == None:
-                jnl += "**Victim Nigga's username: ** `Doesn't own a username!`\n"
+                jnl += "**Spammer's username: ** `Doesn't own a username!`\n"
             elif usname != "None":
-                jnl += "**Victim's username** : @{}\n".format(usname)
+                jnl += "**Spammers's username** : @{}\n".format(usname)
+            if len(gbunVar) > 0:
+                gbunm = "`{}`".format(gbunVar)
+                gbunr = "**Reason: **"+gbunm
+                jnl += gbunr
+            else:
+                jnl += no_reason
+            await reply_message.reply(jnl)
+    else:
+        mention = "`Warning!! User DMCAed By BOSS @CyberJalagam\nReason: Kang Without Permission. `"
+        await event.reply(mention)
+    await event.delete()
          
 
         
