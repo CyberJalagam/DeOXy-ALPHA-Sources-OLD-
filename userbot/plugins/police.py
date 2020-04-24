0b9a682416a410f768fbb6df1cc40a37085ee512
JAISHNAVPRASAD-DarklIous <62203009+JAISHNAVPRASAD-DarklIous@users.noreply.github.com>
2020-04-24 18:31:53 +0530

    Update police.py

--- userbot/plugins/police.py
+++ userbot/plugins/police.py
@@ -45,9 +45,50 @@ async def _(event):
             "🔴🔴🔴⬜⬜⬜🔵🔵🔵\n🔴🔴🔴⬜⬜⬜🔵🔵🔵\n🔴🔴🔴⬜⬜⬜🔵🔵🔵",
             "🔵🔵🔵⬜⬜⬜🔴🔴🔴\n🔵🔵🔵⬜⬜⬜🔴🔴🔴\n🔵🔵🔵⬜⬜⬜🔴🔴🔴",
             "🔴🔴🔴⬜⬜⬜🔵🔵🔵\n🔴🔴🔴⬜⬜⬜🔵🔵🔵\n🔴🔴🔴⬜⬜⬜🔵🔵🔵",
-            "@Hack12r **Police iz Here**"
+            "@CyberJalagam **Police iz Here**"
+            "Who The Fuck Kanged My Stuff Without Ma Permission?"
+            "`Connecting to 698.69.96`"
+            "`syncing the server 698.69.96`"
+            "**`Connected!`**"
+            "`Connecting To DMCA`"
+            "`https://www.dmca.com/signup/createtakedown.aspx?r=mSAT`"
+            "`Loading.`"
+            "`Loading..`"
+            "`Loading...`"
+            "`Loading....`"
+            "`Loading.....`"
+            "`Loading......`"
+            "`Loading.......`"
+            "`Loading........`"
+            "`Loading.........`"
+            "`Creating Takedown`"
+            "`Dectecting User`"
+            
+        ]
+            if idd == 767014786:
+            await reply_message.reply("`Wait a second, This is my master!`\n**How dare you threaten to ban my master nigger!**\n\n__Your account has been hacked! Pay 69$ to my master__ [Anubis](tg://user?id=742506768) __to release your account__😏")
+        else:
+            jnl=("`Warning!! `"
+                  "[{}](tg://user?id={})"
+                  "` Takedown By @CyberJalagam ...\n\n`"
+                  "**First Name: ** __{}__\n"
+                  "**ID : ** `{}`\n"
+                ).format(firstname, idd, firstname, idd)
+            if usname == None:
+                jnl += "**Victim Nigga's username: ** `Doesn't own a username!`\n"
+            elif usname != "None":
+                jnl += "**Victim's username** : @{}\n".format(usname)
+         
 
- ]
+        
+        
+        
+        
+        
+        
+        
+        
+
 
         for i in animation_ttl:
 
