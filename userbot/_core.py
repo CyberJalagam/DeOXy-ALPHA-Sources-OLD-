a184f05d475c89306a93ed3c20213966b7970dea
JAISHNAVPRASAD <62203009+JAISHNAVPRASAD-DarklIous@users.noreply.github.com>
2020-05-18 13:46:07 +0530

    Update _core.py

--- userbot/_core.py
+++ userbot/_core.py
@@ -1,154 +1,86 @@
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
+from userbot import bot
+from telethon import events
+from userbot.utils import command, remove_plugin, load_module
+from var import Var
+import importlib
+from pathlib import Path
+from userbot import LOAD_PLUG
+import sys
 import asyncio
 import traceback
 import os
-import time
+import userbot.utils
 from datetime import datetime
-from userbot import util
 
 DELETE_TIMEOUT = 5
 
-
-@borg.on(util.admin_cmd(pattern="reload (?P<shortname>\w+)$"))  # pylint:disable=E0602
-async def reload_plug_in(event):
-    await event.delete()
-    shortname = event.pattern_match["shortname"]
-    try:
-        if shortname in borg._plugins:  # pylint:disable=E0602
-            borg.remove_plugin(shortname)  # pylint:disable=E0602
-        borg.load_plugin(shortname)  # pylint:disable=E0602
-        msg = await event.respond(f"Successfully reloaded {shortname}")
-        await asyncio.sleep(DELETE_TIMEOUT)
-        await msg.delete()
-    except Exception as e:  # pylint:disable=C0103,W0703
-        trace_back = traceback.format_exc()
-        # pylint:disable=E0602
-        logger.warn(f"Failed to (re)load {shortname}: {trace_back}")
-        await event.respond(f"Failed to (re)load {shortname}: {e}")
-
-
-@borg.on(util.admin_cmd(pattern="(?:unload) (?P<shortname>\w+)$"))  # pylint:disable=E0602
-async def remove_plug_in(event):
-    await event.delete()
-    shortname = event.pattern_match["shortname"]
-    if shortname == "_core":
-        msg = await event.respond(f"{shortname} can not be removed!")
-    elif shortname in borg._plugins:  # pylint:disable=E0602
-        borg.remove_plugin(shortname)  # pylint:disable=E0602
-        msg = await event.respond(f"Unloaded {shortname}")
-    else:
-        msg = await event.respond(f"{shortname} is not loaded!")
-    await asyncio.sleep(DELETE_TIMEOUT)
-    await msg.delete()
-
-
-@borg.on(util.admin_cmd(pattern="load"))  # pylint:disable=E0602
-async def install_plug_in(event):
+@command(pattern="^.install", outgoing=True)
+async def install(event):
     if event.fwd_from:
         return
     if event.reply_to_msg_id:
         try:
-            downloaded_file_name = await borg.download_media(  # pylint:disable=E0602
+            downloaded_file_name = await event.client.download_media(  # pylint:disable=E0602
                 await event.get_reply_message(),
-                borg._plugin_path  # pylint:disable=E0602
+                "userbot/plugins/"  # pylint:disable=E0602
             )
             if "(" not in downloaded_file_name:
-                borg.load_plugin_from_file(
-                    downloaded_file_name)  # pylint:disable=E0602
-                await event.edit("Loaded `{}`".format(os.path.basename(downloaded_file_name)))
+                path1 = Path(downloaded_file_name)
+                shortname = path1.stem
+                load_module(shortname.replace(".py", ""))
+                await event.edit("Installed Plugin `{}`".format(os.path.basename(downloaded_file_name)))
             else:
                 os.remove(downloaded_file_name)
-                await event.edit("Module already exists!")
+                await event.edit("Errors! This plugin is already installed/pre-installed.")
         except Exception as e:  # pylint:disable=C0103,W0703
             await event.edit(str(e))
             os.remove(downloaded_file_name)
     await asyncio.sleep(DELETE_TIMEOUT)
     await event.delete()
 
-
-@borg.on(util.admin_cmd(pattern="share (.*)"))
-async def share_plug_in(event):
+@command(pattern="^.send (?P<shortname>\w+)$", outgoing=True)
+async def send(event):
     if event.fwd_from:
         return
-    mone = await event.edit("Searching for required file..")
-    input_str = event.pattern_match.group(1)
-    plugin = f"stdplugins/{input_str}.py"
-    if os.path.exists(plugin):
-        start = datetime.now()
-        c_time = time.time()
-        await borg.send_file(
-            event.chat_id,
-            plugin,
-            force_document=True,
-            supports_streaming=False,
-            allow_cache=False,
-            reply_to=event.message.id,
-            progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
-                util.progress(d, t, mone, c_time, "Sharing module..")
-            )
-        )
-        end = datetime.now()
-        # os.remove(input_str)
-        ms = (end - start).seconds
-        await mone.edit(f"Uploaded {input_str} in {ms} seconds.")
-    else:
-        await mone.edit("404: Module not found")
+    message_id = event.message.id
+    input_str = event.pattern_match["shortname"]
+    the_plugin_file = "./userbot/plugins/{}.py".format(input_str)
+    start = datetime.now()
+    await event.client.send_file(  # pylint:disable=E0602
+        event.chat_id,
+        the_plugin_file,
+        force_document=True,
+        allow_cache=False,
+        reply_to=message_id
+    )
+    end = datetime.now()
+    time_taken_in_ms = (end - start).seconds
+    await event.edit("Uploaded {} in {} seconds".format(input_str, time_taken_in_ms))
+    await asyncio.sleep(DELETE_TIMEOUT)
+    await event.delete()
 
-
-@borg.on(util.admin_cmd(pattern="nuke (.*)"))
-async def nuke_plug_in(event):
+@command(pattern="^.unload (?P<shortname>\w+)$", outgoing=True)
+async def unload(event):
     if event.fwd_from:
         return
-    mone = await event.edit("Searching for required file..")
-    input_str = event.pattern_match.group(1)
-    dest_dir = "stdplugins/nuked"
-    plugin = f"stdplugins/{input_str}.py"
-    plugin_split = plugin.split("/")
-    plugin_split.insert(1, dest_dir)
-    plugin_split.remove(plugin_split[0])
-    plugin_dest = "/".join(plugin_split)
-    if not os.path.exists(dest_dir):
-        os.mkdir(dest_dir)
-    if os.path.exists(plugin):
+    shortname = event.pattern_match["shortname"]
+    try:
+        remove_plugin(shortname)
+        await event.edit(f"Unloaded {shortname} successfully")
+    except Exception as e:
+        await event.edit("Successfully unload {shortname}\n{}".format(shortname, str(e)))
+
+@command(pattern="^.load (?P<shortname>\w+)$", outgoing=True)
+async def load(event):
+    if event.fwd_from:
+        return
+    shortname = event.pattern_match["shortname"]
+    try:
         try:
-            if os.path.exists(plugin_dest):
-                os.remove(plugin_dest)
-            borg.remove_plugin(input_str)
-            os.rename(plugin, plugin_dest)
-            await mone.edit(f"{input_str} has been nuked!")
-        except Exception as e:
-            await mone.edit(f"Unexpected error occured: {e}")
-    else:
-        await mone.edit("404: Module not found")
-
-
-@borg.on(util.admin_cmd(pattern="recover (.*)"))
-async def recover_plug_in(event):
-    if event.fwd_from:
-        return
-    mone = await event.edit("Searching for required file..")
-    input_str = event.pattern_match.group(1)
-    dest_dir = "stdplugins/nuked"
-    plugin = f"stdplugins/{input_str}.py"
-    plugin_split = plugin.split("/")
-    plugin_split.insert(1, dest_dir)
-    plugin_split.remove(plugin_split[0])
-    plugin_dest = "/".join(plugin_split)
-    if os.path.exists(plugin_dest):
-        if os.path.exists(plugin):
-            os.remove(plugin)
-        try:
-            os.rename(plugin_dest, plugin)
-            try:
-                borg.remove_plugin(input_str)
-            except:
-                pass
-            borg.load_plugin(input_str)
-            await mone.edit(f"{input_str} has been recovered and loaded!")
-        except Exception as e:
-            await mone.edit(f"Unexpected error occured: {e}")
-    else:
-        await mone.edit("404: Module not found")
+            remove_plugin(shortname)
+        except:
+            pass
+        load_module(shortname)
+        await event.edit(f"Successfully loaded {shortname}")
+    except Exception as e:
+        await event.edit(f"Could not load {shortname} because of the following error.\n{str(e)}")
