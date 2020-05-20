421ad98f3e1baeebf46e18101ec5eb16fd305e9b
JAISHNAVPRASAD <62203009+JAISHNAVPRASAD-DarklIous@users.noreply.github.com>
2020-05-20 09:29:06 +0530

    Update police.py

--- userbot/plugins/police.py
+++ userbot/plugins/police.py
@@ -13,7 +13,7 @@ import asyncio
 from uniborg.util import admin_cmd
 from global_variables_sql import SYNTAX, MODULE_LIST
 
-MODULE_LIST.append.("police")
+MODULE_LIST.append("police")
 
 @borg.on(admin_cmd(pattern=r"(.*)"))
 
